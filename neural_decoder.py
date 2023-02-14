fe8ccfe70a269cad8ce1884a089b0199e28aa31f
KitchenTable99 <ccbitt23@colby.edu>
2023-02-14 14:29:17 -0500

    finished softmax and calebs part for nonlinear

--- neural_decoder.py
+++ neural_decoder.py
@@ -6,7 +6,11 @@ Project 1: Hebbian Learning
 
 NOTE: Your challenge is to NOT import numpy here!
 '''
+import random
+
 import tensorflow as tf
+import numpy as np
+import time
 
 
 class NeuralDecoder:
@@ -26,8 +30,9 @@ class NeuralDecoder:
         NOTE: Remember to wrap your weights and bias as tf.Variables for gradient tracking!
         '''
         # Change/set these
-        self.wts = tf.Variable(tf.random.normal(shape = (num_features,num_classes), stddev = wt_stdev))
-        self.b = tf.Variable(tf.random.normal(shape = (num_classes,), stddev = wt_stdev))
+        self.wts = tf.Variable(tf.random.normal(shape=(num_features, num_classes), stddev=wt_stdev))
+        self.b = tf.Variable(tf.random.normal(shape=(num_classes,), stddev=wt_stdev))
+        self.num_classes = num_classes
 
     def get_wts(self):
         '''Returns the decoder wts'''
@@ -55,22 +60,6 @@ class NeuralDecoder:
         '''
         self.b = b
 
-    def one_hot(self, y, C, off_value=0):
-        '''One-hot codes the vector of class labels `y`
-
-        Parameters:
-        -----------
-        y: tf.constant. shape=(B,) int-coded class assignments of training mini-batch. 0,...,numClasses-1
-        C: int. Number of unique output classes total
-        off_value: int. The "off" value that represents all other values in each sample's one-hot vector that is not 1.
-
-        Returns:
-        -----------
-        y_one_hot: tf.constant. tf.float32. shape=(B, C) One-hot coded class assignments.
-            e.g. if off_value=-1, y=[1, 0], and C=3, the one-hot vector would be:
-            [[-1., 1., -1.], [-1., 1., -1.]]
-        '''
-        return tf.one_hot(y, C, off_value = off_value, dtype = 'float32')
 
     def accuracy(self, y_true, y_pred):
         '''Computes the accuracy of classified samples. Proportion correct
@@ -89,7 +78,7 @@ class NeuralDecoder:
         same = tf.where(y_true == y_pred)
         return tf.shape(same)[0]/tf.shape(y_pred)[0]
 
-    def forward(self, x):
+    def forward(self, x, net_act=False):
         '''Performs the forward pass through the decoder network with data samples `x`
 
         Parameters:
@@ -133,10 +122,10 @@ class NeuralDecoder:
         -----------
         y_preds: tf.constant. shape=(B,). int-coded predicted class for each sample in the mini-batch.
         '''
-        if net_act == None:
+        if net_act is None:
             net_act = self.forward(x)
 
-        return tf.math.argmax(net_act, axis = 1)
+        return tf.math.argmax(net_act, axis=1)
 
     def early_stopping(self, recent_val_losses, curr_val_loss, patience):
         '''Helper method used during training to determine whether training should stop before the maximum number of
@@ -181,7 +170,7 @@ class NeuralDecoder:
         recent_val_losses.append(curr_val_loss)
         if len(recent_val_losses) > patience:
             recent_val_losses = recent_val_losses[1:patience+1]
-            if recent_val_losses.index(min(recent_val_losses)) == 0:
+            if np.argmin(recent_val_losses) == 0:
                 stop = True
 
         return recent_val_losses, stop
@@ -203,10 +192,10 @@ class NeuralDecoder:
 
         Hint: Check out tf.gather. See TF tutorial from last semester (end of final notebook) for example usage.
         '''
-        pass
+        return tf.gather(x, indices)
 
     def fit(self, x, y, x_val=None, y_val=None, mini_batch_sz=512, lr=1e-4, max_epochs=1000, patience=3, val_every=1,
-            verbose=True):
+            verbose=True, print_every=100):
         '''Trains the neural decoder on the training samples `x` (and associated int-coded labels `y`) using early
         stopping and the Adam optimizer.
 
@@ -239,26 +228,61 @@ class NeuralDecoder:
 
         TODO:
         Go through the usual motions:
-        - Set up Adam optimizer and loss history tracking containers.
         - In each epoch setup mini-batch. You can sample with replacement or without replacement (shuffle) between epochs
         (your choice).
         - Compute forward pass and loss for each mini-batch. Have your Adam optimizer apply the gradients to update the
         wts and bias.
         - Record the average training loss values across all mini-batches in each epoch.
-        - If we're on the first, max, or an appropriate epoch, check the validation set accuracy and loss.
-        Check for early stopping with the val loss.
         '''
         N, M = x.shape
+        one_hot_y = self.one_hot(y, self.num_classes)
+        one_hot_y_val = self.one_hot(y_val, self.num_classes)
 
         # Define loss tracking containers
         train_loss_hist = []
         val_loss_hist = []
         recent_val_loss_hist = []
+        stop = False
+
+        for n in range(max_epochs):
+            t_0 = time.time()
+
+            with tf.GradientTape() as tape:
+                batch_idx = random.sample(range(0, len(x) - 1), mini_batch_sz)
+                batch_x = self.extract_at_indices(x, batch_idx)
+                batch_y = self.extract_at_indices(one_hot_y, batch_idx)
+                net_in = self.forward(batch_x)
+                loss = self.loss(batch_y, net_in)
+                loss = tf.reduce_mean(loss)
+                train_loss_hist.append(loss)
+
+            grads = tape.gradient(loss, [self.wts, self.b])
+            opt = tf.keras.optimizers.Adam(lr)
+            opt.apply_gradients(zip(grads, [self.wts, self.b]))
+
+            if verbose and n % val_every == 0:
+                val_net_in = self.forward(x_val)
+                val_loss = self.loss(one_hot_y_val, val_net_in)
+                val_loss = tf.reduce_mean(val_loss)
+                val_loss_hist.append(val_loss)
+                recent_val_loss_hist.append(val_loss)
+                recent_val_loss_hist, stop = self.early_stopping(recent_val_loss_hist, val_loss, patience=patience)
+
+            if stop:
+                break
+
+            t = time.time() - t_0
+            if n == 0:
+                print (f'Epoch 0/{max_epochs-1} completed \n 1 epoch took {t:.2f} seconds. Expected runtime is {t*(max_epochs-n):.2f} seconds.')
+            elif n % print_every == 0:
+                print (f'Epoch {n}/{max_epochs-1} completed, took {t:.2f} seconds. Approx. {int(t*(max_epochs-n))} seconds remain.')
+
+        return train_loss_hist, val_loss_hist, n
 
 
 # Suggested that SoftmaxDecoder and NonlinearDecoder go below:
 class SoftmaxDecoder(NeuralDecoder):
-    def forward(self, x):
+    def forward(self, x, net_act=False):
         '''Performs the forward pass through the decoder network with data samples `x`
 
         Parameters:
@@ -269,10 +293,13 @@ class SoftmaxDecoder(NeuralDecoder):
         -----------
         net_act: tf.constant. shape=(B, C). Network activation to every sample in the mini-batch.
         '''
-        net_act = x @ self.wts + self.b
-        return tf.nn.softmax(net_act)
+        wts = tf.cast(self.wts, tf.double)
+        b = tf.cast(self.b, tf.double)
+        net_in = tf.matmul(x, wts) + b
 
-    def loss(self, yh, net_act):
+        return tf.nn.softmax(net_in) if net_act else net_in
+
+    def loss(self, yh, net_in):
         '''Computes the loss on the current mini-batch using the one-hot coded class labels `yh` and `net_act`.
 
         Parameters:
@@ -285,6 +312,44 @@ class SoftmaxDecoder(NeuralDecoder):
         loss: float. Loss computed over the mini-batch.
 
         '''
-        B = tf.cast(tf.shape(yh)[0], 'float32')
-        sum = tf.reduce_sum(yh*tf.math.log(net_act))
-        return (-1/B)*sum
+        return tf.nn.softmax_cross_entropy_with_logits(yh, net_in)
+
+    def one_hot(self, y, C):
+        '''One-hot codes the vector of class labels `y`
+
+        Parameters:
+        -----------
+        y: tf.constant. shape=(B,) int-coded class assignments of training mini-batch. 0,...,numClasses-1
+        C: int. Number of unique output classes total
+        off_value: int. The "off" value that represents all other values in each sample's one-hot vector that is not 1.
+
+        Returns:
+        -----------
+        y_one_hot: tf.constant. tf.float32. shape=(B, C) One-hot coded class assignments.
+            e.g. if off_value=-1, y=[1, 0], and C=3, the one-hot vector would be:
+            [[-1., 1., -1.], [-1., 1., -1.]]
+        '''
+        return tf.one_hot(y, C, off_value=0, dtype='float32')
+
+
+class NonlinearDecoder(NeuralDecoder):
+
+    def __init__(self, num_features, num_classes, wt_stdev=.1, beta=.005, loss_exp=6):
+        super().__init__(num_features, num_classes, wt_stdev)
+        self.beta = beta
+        self.loss_exp = loss_exp
+
+    def forward(self, x, net_act=False):
+        net_in = tf.matmul(x, self.wts) + self.b
+
+        return tf.nn.tanh(tf.math.scalar_mul(self.beta, net_in)) if net_act else net_in
+
+    def one_hot(self, y, C):
+        return tf.one_hot(y, C, off_value=-1, dtype='float32')
+
+    def loss(self, yh, net_act):
+        diffs = tf.math.subtract(yh, net_act)
+        abs_diffs = tf.math.abs(diffs)
+        exp_diffs = tf.math.pow(abs_diffs, self.loss_exp)
+
+        return tf.reduce_sum(exp_diffs)
